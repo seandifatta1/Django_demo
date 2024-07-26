081c3f975edcc3999d092401ba06786776b0b8ae
Sean DiFatta <seandifatta1@gmail.com>
2024-07-26 16:23:10 -0400

    clicking the team now adds it as the users favorite team and redirects the user to the team specific page

--- team/views.py
+++ team/views.py
@@ -8,9 +8,11 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+
 
 def team_view(request, team_name):
-    return HttpResponse(f"Welcome to the {team_name} page!")
+    return render(request, 'team/team_detail.html', {'team_name': team_name})
 
 
 def team_list(request):
@@ -27,15 +29,26 @@ def team_list(request):
     return render(request, 'team/team_list.html', {'teams_by_division': teams_by_division})
 
 
-
 # @login_required
 def handle_click(request):
     if request.method == 'POST':
         image_name = request.POST.get('image_name')
         if image_name:
+
+            if "team_logos/" in image_name:
+                team_name = image_name.split("/")[1]
+            else:
+                team_name = image_name
+
+            # Extract team name from image_name
+            team_name = team_name.split(".")[0]
+
             # Update the favorite_team field for the current user
             user = request.user
-            user.favorite_team = image_name
+            user.favorite_team = team_name
             user.save()
-            return JsonResponse({'status': 'success', 'image_name': image_name})
+
+            # Redirect to the team-specific page
+            # return redirect(f'/teams/{team_name}/')
+            return JsonResponse({'status': 'success', 'team_name': team_name})
     return JsonResponse({'status': 'failed'})
