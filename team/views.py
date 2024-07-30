408bfe416e91d3d48cbed853339d5a347a904f19
Sean DiFatta <seandifatta1@gmail.com>
2024-07-30 12:11:32 -0400

    recovered

--- team/views.py
+++ team/views.py
@@ -10,16 +10,18 @@ from .models import Team
 
 User = get_user_model()
 
+import random
+
 def team_detail(request, team_name):
     user = request.user
     favorite_team = user.favorite_team
 
     team_name = team_name[0].upper() + team_name[1:].lower()
-    # team = get_object_or_404(Team, name=team_name)
     team = get_object_or_404(Team, name=favorite_team)
 
     teams_in_division = Team.objects.filter(division=team.division).order_by('-wins')
-    opponent = random.choice([t for t in teams_in_division if t.name != team_name])
+    all_teams = Team.objects.exclude(name=team.name)
+    opponent = random.choice(all_teams)
 
     context = {
         'favorite_team': favorite_team,
@@ -30,17 +32,26 @@ def team_detail(request, team_name):
 
     return render(request, 'team/team_detail.html', context)
 
+
 def team_list(request):
-    teams = Team.objects.all().values('division', 'logo_path')
+    teams = Team.objects.all().values('division', 'logo_path', 'conference')
 
-    teams_by_division = defaultdict(list)
+    afc_teams_by_division = defaultdict(list)
+    nfc_teams_by_division = defaultdict(list)
+
     for team in teams:
-        teams_by_division[team['division']].append(team['logo_path'])
+        if team['conference'] == 'AFC':
+            afc_teams_by_division[team['division']].append(team['logo_path'])
+        elif team['conference'] == 'NFC':
+            nfc_teams_by_division[team['division']].append(team['logo_path'])
 
-    teams_by_division = dict(teams_by_division)
+    afc_teams_by_division = dict(afc_teams_by_division)
+    nfc_teams_by_division = dict(nfc_teams_by_division)
 
-    return render(request, 'team/team_list.html', {'teams_by_division': teams_by_division})
-
+    return render(request, 'team/team_list.html', {
+        'afc_teams_by_division': afc_teams_by_division,
+        'nfc_teams_by_division': nfc_teams_by_division
+    })
 def handle_click(request):
     if request.method == 'POST':
         image_name = request.POST.get('image_name')
